8184359983c7badfb659a778fbfa38f269da580f
patsul12 <patsul12@gmail.com>
2016-01-17 13:18:10 -0800

    turn off typed on smaller screens;

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,8 +1,10 @@
 $(function(){
+  if($(window).width() >= 590) {
     $(".typed-section").typed({
       strings: ["Developer", "Coffee-Addict", "Dog-Lover", "Free-Thinker", "Nature-Enthusiast"],
       typeSpeed: 50,
       backSpeed: 50,
       loop: true,
     });
+  }
 });
