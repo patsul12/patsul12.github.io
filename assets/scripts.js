010b38d5ce381cb4d3529eebae2250f6cd3b3a9d
patsul12 <patsul12@gmail.com>
2016-01-17 13:11:52 -0800

    add typed js to the header

---
+++ assets/scripts.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".typed-section").typed({
+      strings: ["Developer", "Coffee-Addict", "Dog-Lover", "Free-Thinker", "Nature-Enthusiast"],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+    });
+});
