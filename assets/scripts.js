605b11d79e7bc13eb6a7b3f0925eb7b36e847efa
patsul12 <patsul12@gmail.com>
2016-05-01 17:23:29 -0700

    working with creating a patricle background in canvas for homepage

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,20 @@
+function draw() {
+  var canvas = document.getElementById('canvas');
+  var height = $(document).height();
+  var width  = $(document).width();
+  
+  canvas.height = height;
+  canvas.width  = width;
+
+  var ctx = canvas.getContext('2d');
+  // draw circle
+  ctx.beginPath();
+  ctx.fillStyle="black";
+  ctx.arc(100,20,10,0, Math.PI*2, true);
+  ctx.closePath();
+  ctx.fill();
+}
+
 $(function(){
   if($(window).width() >= 590) {
     $(".typed-section").typed({
@@ -7,4 +24,8 @@ $(function(){
       loop: true,
     });
   }
+
+  draw();
 });
+
+
